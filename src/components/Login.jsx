3e1e9bda004a20ddf0716a32217085889941b96f
Khanh <kquoc29200@gmail.com>
2024-05-06 21:58:46 +0700

    update

---
+++ src/components/Login.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Box, CardMedia } from "@mui/material";
+
+import { Videos, ChannelCard } from ".";
+import { loginAPI, loginFacebookAPI } from "../utils/fetchFromAPI";
+import ReactFacebookLogin from "react-facebook-login";
+
+const Login = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+
+  useEffect(() => {}, []);
+  const navigate = useNavigate();
+  return (
+    <div className="p-5 " style={{ minHeight: "100vh" }}>
+      <div className=" d-flex justify-content-center">
+        <form className="row g-3 text-white">
+          <div className="col-md-12">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+
+          <div className="col-md-12">
+            <label htmlFor="inputEmail4" className="form-label">
+              Password
+            </label>
+            <input className="form-control" id="pass" />
+          </div>
+          <div className="col-12">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => {
+                let txtEmail = document.querySelector("#email").value;
+                let txtPassword = document.querySelector("#pass").value;
+
+                let model = {
+                  email: txtEmail,
+                  password: txtPassword,
+                };
+
+                loginAPI(model)
+                  .then((result) => {
+                    alert(result.message);
+
+                    localStorage.setItem("LOGIN_USER", result.data);
+                    window.location.reload();
+                  })
+                  .catch((error) => {
+                    alert(error.response.data.message);
+                  });
+              }}
+            >
+              Login
+            </button>
+            <a
+              className="text-primary"
+              href="#"
+              onClick={() => navigate("/forget")}
+            >
+              {" "}
+              Forget password
+            </a>
+          </div>
+
+          <ReactFacebookLogin
+            appId="797783445614267"
+            fields="name,email,picture"
+            callback={(response) => {
+              console.log(response);
+              // call API login facebook
+              let model = {
+                userID: response.userID,
+                email: response.email,
+                name: response.name,
+              };
+              loginFacebookAPI(model).then((result) => {
+                alert("Login thành công");
+
+                localStorage.setItem("LOGIN_USER", result.data);
+                window.location.reload();
+              });
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,172 @@
+import axios from "axios";
+
+export const BASE_URL = "http://localhost:8080";
+export const BASE_URL_IMG = "http://localhost:8080/public/img";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    token: localStorage.getItem("LOGIN_USER"),
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
+
+export const getVideoAPI = async () => {
+  const { data } = await axios.get(`${BASE_URL}/video/get-video`, options);
+
+  return data.data;
+};
+
+export const getVideoPageAPI = async (page) => {
+  const { data } = await axios.get(
+    `${BASE_URL}/video/get-video-page/${page}`,
+    options
+  );
+
+  return data.data; // =>{listVideo, totalPage}
+};
+
+export const getVideoTypeAPI = async () => {
+  const { data } = await axios.get(`${BASE_URL}/video/get-video-type`, options);
+
+  return data.data;
+};
+
+export const getVideoWithTypeAPI = async (typeId) => {
+  const { data } = await axios.get(
+    `${BASE_URL}/video/get-video-with-type/${typeId}`,
+    options
+  );
+
+  return data.data;
+};
+
+export const getVideoDetailAPI = async (videoId) => {
+  const { data } = await axios.get(
+    `${BASE_URL}/video/get-video-detail/${videoId}`,
+    options
+  );
+
+  return data.data; // =>{listVideo, totalPage}
+};
+
+export const signUpAPI = async (model) => {
+  const { data } = await axios.post(`${BASE_URL}/user/sign-up`, model, options);
+
+  return data; // {code,data,message,date}
+};
+
+export const loginAPI = async (model) => {
+  const { data } = await axios.post(`${BASE_URL}/user/login`, model, options);
+
+  return data; // {code,data,message,date}
+};
+
+// API get comment
+export const getCommentAPI = async (videoId) => {
+  const { data } = await axios.get(
+    `${BASE_URL}/video/get-comment/${videoId}`,
+    options
+  );
+
+  return data.data; // =>{listVideo, totalPage}
+};
+
+export const commentAPI = async (model) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/video/comment`,
+    model,
+    options
+  );
+
+  return data.data; // =>{listVideo, totalPage}
+};
+
+export const loginFacebookAPI = async (model) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/user/login-facebook`,
+    model,
+    options
+  );
+
+  return data; // {code,data,message,date}
+};
+
+export const checkForgetEmailAPI = async (model) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/user/forget-check-mail`,
+    model,
+    options
+  );
+
+  return data; // {code,data,message,date}
+};
+
+export const checkForgetCodelAPI = async (model) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/user/forget-check-code`,
+    model,
+    options
+  );
+
+  return data; // {code,data,message,date}
+};
+
+export const uploadCloudAPI = async (formData) => {
+  const { data } = await axios.post(
+    `https://api.cloudinary.com/v1_1/dghvdbogx/upload`,
+    formData
+  );
+
+  return data;
+};
+
+export const uploadAvatarAPI = async (formData) => {
+  const { data } = await axios.post(
+    `${BASE_URL}/user/upload-avatar`,
+    formData,
+    options
+  );
+
+  return data; // {code,data,message,date}
+};
+
+export const getUserAPI = async () => {
+  const { data } = await axios.get(`${BASE_URL}/user/get-all-user/`, options);
+  return data;
+};
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    // console.log(error.response)
+    if (
+      error.response.status == 401 &&
+      error.response.data == "TokenExpiredError"
+    ) {
+      // API reste token
+      axios
+        .post(`${BASE_URL}/user/reset-token`, null, options)
+        .then((result) => {
+          localStorage.setItem("LOGIN_USER", result.data.data);
+          window.location.reload();
+        })
+        .catch((error) => {
+          // logout
+        });
+    }
+
+    return Promise.reject(error);
+  }
+);
